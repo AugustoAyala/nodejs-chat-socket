4fd7d6dc164188eb3490b3d29d3d304f47d4af94
Augusto Ayala <50784512+AugustoAyala@users.noreply.github.com>
2019-07-29 23:23:16 -0300

    Update main.js

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,18 +2,18 @@ $(function(){
     
     const socket = io();
 
-    const Form = $('#form-mensaje');
-    const Bodychat = $('#body-chat');
-    const Mensaje = $('#input-mensaje');
+    const form = $('#form-mensaje');
+    const bodyChat = $('#body-chat');
+    const mensaje = $('#input-mensaje');
     
-    Form.submit( e => {
+    form.submit( e => {
         e.preventDefault();
-        socket.emit('server mensaje', Mensaje.val());
-        Mensaje.val("");
+        socket.emit('server mensaje', mensaje.val());
+        mensaje.val("");
     });
 
     socket.on('view mensaje', function (data) {
-        Bodychat.append(data.msg + '<br/>');
+        bodyChat.append(data.msg + '<br/>');
     });
 
     
