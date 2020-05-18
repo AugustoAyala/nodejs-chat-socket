1d2f6c3cd169b7855cff49b5454723edfea9b67f
Augusto Ayala <50784512+AugustoAyala@users.noreply.github.com>
2020-05-18 19:41:24 -0300

    Update conexion.js

--- src/conexion.js
+++ src/conexion.js
@@ -1,14 +1,10 @@
 module.exports = function(io) {
 
-
 io.on('connection', socket => {
-	
-
-	socket.on('server mensaje', function (data) {
-		io.sockets.emit('view mensaje', {
-			msg: data,
-		});	
-
-	});
-});
+    socket.on('server mensaje', function (data) {
+        io.sockets.emit('view mensaje', {
+	    msg: data,
+	});	
+     });
+  });
 }
